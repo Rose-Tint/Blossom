41c51f6932a41620210fe810f83456b0be96e29d
Rose <71909018+Xenovia02@users.noreply.github.com>
2022-09-14 15:04:02 -0500

    began cmd options

--- src/Blossom/Cmd.hs
+++ src/Blossom/Cmd.hs
@@ -1,3 +1,25 @@
 module Blossom.Cmd (
-
+    CommandLine(..),
+    Verbosity(..),
+    parseCommandLine,
 ) where
+
+import Options.Applicative
+
+
+data CommandLine = CommandLine {
+    cmdSourceFiles :: [FilePath]
+    }
+
+
+parseCommandLine :: IO CommandLine
+parseCommandLine = execParser commandLineInfo
+
+commandLineInfo :: ParserInfo CommandLine
+commandLineInfo = info
+    (helper <*> commandLine)
+    fullDesc
+
+commandLine :: Parser CommandLine
+commandLine = CommandLine
+    <$> many (strArgument mempty)
