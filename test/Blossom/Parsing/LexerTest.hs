beca969759b5eb1880068aff5033f295279e6545
Rose <71909018+Xenovia02@users.noreply.github.com>
2022-09-14 17:42:36 -0500

    added more tests

--- test/Blossom/Parsing/LexerTest.hs
+++ test/Blossom/Parsing/LexerTest.hs
@@ -12,7 +12,7 @@ import Blossom.Parsing.Token
 tests :: Test
 tests = TestLabel "Blossom.Parsing.Lexer" $ TestList [
     TestLabel "Function (tokenize)" $ TestList [
-        TestLabel "integers (TokInteger):" $ TestCase $
+        TestLabel "integers" $ TestCase $
             let actual = tokenize (BSC.pack "947235 3 51333332")
                 expected = Right [
                     TokInteger 947235,
@@ -20,7 +20,7 @@ tests = TestLabel "Blossom.Parsing.Lexer" $ TestList [
                     TokInteger 51333332
                     ]
             in expected @=? actual,
-        TestLabel "floats (TokFloat):" $ TestCase $
+        TestLabel "floats" $ TestCase $
             let actual = tokenize (BSC.pack "3.14159 512.4 314.2634")
                 expected = Right [
                     TokFloat 3.14159,
@@ -28,33 +28,53 @@ tests = TestLabel "Blossom.Parsing.Lexer" $ TestList [
                     TokFloat 314.2634
                     ]
             in expected @=? actual,
-    -- | TokString BS.ByteString
-    -- | TokOperator LLVM.Name
-    -- | TokSmallId LLVM.Name
-    -- | TokBigId LLVM.Name
-    -- | TokSemi
-    -- | TokColon
-    -- | TokDoubleColon
-    -- | TokArrow
-    -- | TokEqArrow
-    -- | TokEnd
-        TestCase $
-            let actual = tokenize (BSC.pack "aBc4' 123 45.6 Def'9")
+        TestLabel "strings" $ TestCase $
+            -- TODO: allow escaped quotations
+            let actual = tokenize (BSC.pack "\"foo\" \"bar\" \"\"")
                 expected = Right [
-                    mkSmallId "aBc4'",
-                    TokInteger 123,
-                    TokFloat 45.6,
-                    mkBigId "Def'9"
+                    TokString (BSC.pack "foo"),
+                    TokString (BSC.pack "bar"),
+                    TokString BSC.empty
                     ]
             in expected @=? actual,
-        TestCase $
-            let actual = tokenize (BSC.pack "aBc4' 123 45.6 Def'9")
+        TestLabel "operators" $ TestList [], -- TODO
+        TestLabel "small identifiers" $ TestCase $
+            let actual = tokenize (BSC.pack "fo'0 bar' _FooBar")
                 expected = Right [
-                    mkSmallId "aBc4'",
-                    TokInteger 123,
-                    TokFloat 45.6,
-                    mkBigId "Def'9"
+                    mkSmallId "fo'0",
+                    mkSmallId "bar'",
+                    mkSmallId "_FooBar"
                     ]
-            in expected @=? actual
+            in expected @=? actual,
+        TestLabel "big identifiers" $ TestCase $
+            let actual = tokenize (BSC.pack "F0o Bar' Foo'Bar")
+                expected = Right [
+                    mkBigId "F0o",
+                    mkBigId "Bar'",
+                    mkBigId "Foo'Bar"
+                    ]
+            in expected @=? actual,
+        -- TODO: (TokSemi, TokColon, TokDoubleColon, TokArrow, TokEqArrow)
+        TestLabel "reserved operators and keywords" $ TestList [],
+        TestLabel "mixed" $ TestList [
+            TestCase $
+                let actual = tokenize (BSC.pack "aBc4' 123 45.6 Def'9")
+                    expected = Right [
+                        mkSmallId "aBc4'",
+                        TokInteger 123,
+                        TokFloat 45.6,
+                        mkBigId "Def'9"
+                        ]
+                in expected @=? actual,
+            TestCase $
+                let actual = tokenize (BSC.pack "aBc4' 123 45.6 Def'9")
+                    expected = Right [
+                        mkSmallId "aBc4'",
+                        TokInteger 123,
+                        TokFloat 45.6,
+                        mkBigId "Def'9"
+                        ]
+                in expected @=? actual
+            ]
         ]
     ]
