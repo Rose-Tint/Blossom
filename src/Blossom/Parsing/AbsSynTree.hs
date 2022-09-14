97a9b1dd1d21fc094a0b13b3820e6360addac120
Rose <71909018+Xenovia02@users.noreply.github.com>
2022-09-14 17:43:11 -0500

    began to form the AST structure

--- src/Blossom/Parsing/AbsSynTree.hs
+++ src/Blossom/Parsing/AbsSynTree.hs
@@ -1,5 +1,44 @@
 module Blossom.Parsing.AbsSynTree (
-    AbsSynTree(..),
+    ModuleAST(..),
+    Import(..),
+    TopLevel(..),
+    Function(..),
+    Param(..),
+    Data(..),
+    Constructor(..),
 ) where
 
-data AbsSynTree = AbsSynTree
+import qualified LLVM.AST as LLVM
+
+
+data ModuleAST = ModuleAST {
+    moduleImports :: [Import],
+    moduleTopExprs :: [TopLevelExpr]
+    }
+
+newtype Import = Import LLVM.Name
+
+data TopLevel
+    = FuncDef Function
+    | DataDef Data
+
+data Function = Function {
+    funcName :: LLVM.Name,
+    funcParams :: [Param],
+    funcBody :: FunctionBody
+    }
+
+data Param = Param {
+    paramName :: LLVM.Name
+    -- paramType ::
+    }
+
+data Data = Data {
+    dataName :: LLVM.Name,
+    dataCtors :: [Constructor],
+    }
+
+data Constructor = Constructor {
+    ctorName :: LLVM.Name,
+    ctorTypes :: [LLVM.Name]
+    }
