ab3568e4059fb516581f913446bccce38184dec0
Rose <71909018+Xenovia02@users.noreply.github.com>
2022-10-13 14:58:12 -0500

    added pretty printing to the AST

--- src/Blossom/Parsing/AbsSynTree.hs
+++ src/Blossom/Parsing/AbsSynTree.hs
@@ -15,7 +15,24 @@ module Blossom.Parsing.AbsSynTree (
 
 import Blossom.Common.Name (Ident)
 import Blossom.Common.Literal (Literal)
-import Prettyprinter (Pretty(pretty), hsep, (<+>), parens)
+import Blossom.Common.Source (HasLoc(..), mergeLocs)
+import Prettyprinter (
+    Pretty(pretty),
+    PageWidth(..),
+    (<+>),
+    brackets,
+    hsep,
+    line,
+    nest,
+    sep,
+    vsep,
+    backslash,
+    braces,
+    colon,
+    equals,
+    parens,
+    pageWidth,
+    )
 
 
 data ModuleAST = ModuleAST {
@@ -51,7 +68,7 @@ data Expr
     -- Shunting-Yard algorithm will figure the application
     -- order after parsing.
     | FuncApp [Expr]
-    | Lambda [Pattern] Expr
+    | Lambda Params Expr
     | Match Expr [Case]
     | TypedExpr Expr Type
     deriving (Show, Eq)
@@ -81,7 +98,67 @@ data Type
     | Type :-> Type
     deriving (Show, Eq)
 
+instance Pretty ModuleAST where
+    pretty (ModuleAST imports exprs) =
+        lineBreak <>
+        vsep (map pretty imports) <> line <>
+        vsep (map ((<> line) . pretty) exprs) <>
+        lineBreak
+        where
+            lineBreak = pageWidth (\pw -> brackets $ case pw of
+                AvailablePerLine w _ -> pretty (replicate (w - 2) '~')
+                Unbounded -> pretty (replicate 78 '~')
+                )
+
+instance Pretty Import where
+    pretty (Import mdl) = "import" <+> pretty mdl
+
+instance Pretty TopLevelExpr where
+    pretty (FuncDecl ident typ) =
+        "func" <+> pretty ident <> colon <+> pretty typ
+    pretty (FuncDef ident params body) =
+        "func" <+> pretty ident <+> sep (map pretty params)
+            <> nest 4 (line <> equals <+> pretty body)
+    pretty (DataDef ident ctors) =
+        "data" <+> pretty ident <+> braces (nest 4 (line <>
+            vsep (map pretty ctors)
+            ) <> line)
+
+instance Pretty Pattern where
+    pretty (Param ident) = pretty ident
+    pretty (CtorPtrn ident []) = parens (pretty ident)
+    pretty (CtorPtrn ident args) = parens $
+        pretty ident <+> hsep (map pretty args)
+
+instance Pretty Expr where
+    pretty (VarExpr ident) = pretty ident
+    pretty (LitExpr lit) = pretty lit
+    pretty (FuncApp vals) = hsep (map (\val -> case val of
+        FuncApp{} -> parens (pretty val)
+        Lambda{} -> parens (pretty val)
+        TypedExpr{} -> parens (pretty val)
+        _ -> pretty val
+        ) vals)
+    pretty (Lambda params body) =
+        backslash <> hsep (map pretty params) <+> "=>" <+> pretty body
+    pretty (Match expr cases) = "match" <+> pretty expr <+> braces
+        (nest 4 (line <> vsep (map pretty cases)))
+    pretty (TypedExpr expr typ) = pretty expr <+> colon <+> pretty typ
+
+instance Pretty Case where
+    pretty (Case expr body) = pretty expr <+> "=>" <+> pretty body
+
+instance Pretty Constructor where
+    pretty (Constructor ident typ) = pretty ident <+> colon <+> pretty typ
+    pretty (Nullary ident) = pretty ident
+
 instance Pretty Type where
+    pretty (TypeCon name []) = pretty name
     pretty (TypeCon name args) = pretty name <+> hsep (map pretty args)
     pretty (t1@(:->){} :-> t2) = parens (pretty t1) <+> "->" <+> pretty t2
     pretty (t1 :-> t2) = pretty t1 <+> "->" <+> pretty t2
+
+instance HasLoc Pattern where
+    getLoc (Param iden) = getLoc iden
+    getLoc (CtorPtrn ctor []) = getLoc ctor
+    getLoc (CtorPtrn ctor args) = mergeLocs ctor (last args)
