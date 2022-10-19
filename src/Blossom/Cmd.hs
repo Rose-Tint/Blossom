6b4bfec103fa47172b681a68a3a56d3a02545cb3
Rose <71909018+Xenovia02@users.noreply.github.com>
2022-10-19 12:55:20 -0500

    Began the Blossom monad

--- src/Blossom/Cmd.hs
+++ src/Blossom/Cmd.hs
@@ -1,28 +1,39 @@
 module Blossom.Cmd (
     CmdLine(..),
+    Verbosity(..),
     parseCmdLine,
 ) where
 
 import Options.Applicative (
+    strArgument,
+    short,
+    metavar,
+    long,
+    info,
+    helper,
+    help,
+    fullDesc,
+    execParser,
     Alternative(many),
     Parser,
     ParserInfo,
-    execParser,
-    fullDesc,
-    help,
-    helper,
-    info,
-    long,
-    metavar,
-    short,
-    strArgument,
-    switch,
+    maybeReader,
+    option,
+    ReadM, value,
     )
+import Data.Char (toLower, digitToInt, isDigit)
 
+
+data Verbosity
+    = Silent
+    | ErrorsOnly
+    | Normal
+    | Verbose
+    deriving (Show, Eq, Ord, Enum)
 
 data CmdLine = CmdLine {
     cmdSourceFiles :: [FilePath],
-    cmdVerbose :: Bool
+    cmdVerbosity :: Verbosity
     }
 
 
@@ -39,8 +50,25 @@ cmdLineParser = CmdLine
     <$> many (strArgument (
             metavar "files"
         ))
-    <*> switch (
+    <*> option verbosityReader (
             short 'v'
             <> long "verbose"
             <> help "Enable more detailed output"
+            <> value Normal
         )
+
+verbosityReader :: ReadM Verbosity
+verbosityReader = maybeReader go
+    where
+        readNum [] = Nothing
+        readNum (ch : chs)
+            | not (isDigit ch) = Nothing
+            | null chs = Just (digitToInt ch)
+            | otherwise = (digitToInt ch +) . (* 10) <$> readNum chs
+        go str = case map toLower str of
+            "silent" -> Just Silent
+            "errors" -> Just ErrorsOnly
+            "normal" -> Just Normal
+            "verbose" -> Just Verbose
+            _ -> toEnum <$> readNum str
+
